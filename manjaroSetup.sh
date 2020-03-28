05da5b47bc93c91f4f96c1bb78cbac9bf53a220a
theHumanGiraffe <vmorganp@gmail.com>
2020-03-28 16:37:35 -0500

    added one for manjaro

---
+++ manjaroSetup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# general updates
+pacman -Syu
+
+# apps I like
+sudo pacman -S zsh discord code virtualbox git --noconfirm
+
+# zsh
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+chsh -s /usr/bin/zsh
+cp .zshrc ~/.zshrc
+
+# snap
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+# other stuff I like that requires snap
+sudo snap install spotify
+sudo snap install signal-desktop
+
+
+
+
